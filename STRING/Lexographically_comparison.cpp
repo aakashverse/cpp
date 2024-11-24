1ea60643d173be34d99032c6a1e28a974eac3edf
planet-akash <akashyadavakash03@gmail.com>
2024-11-24 11:51:19 +0530

    Lexographical added

---
+++ STRING/Lexographically_comparison.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Lexographical comparison are the comparison on the basis of occurence of characters in dictionaries
+int main(){
+    string s="aaaa";
+    string t="aaaA";
+    
+    transform(s.begin(),s.end(),s.begin(), ::tolower);
+    transform(t.begin(),t.end(),t.begin(), ::tolower);
+    
+    if(s==t) cout<<0<<endl;
+    
+    else if(s<t) cout<<-1<<endl;
+    
+    else cout<<1<<endl;
+}
