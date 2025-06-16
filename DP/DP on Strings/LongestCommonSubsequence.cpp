9db1db482e4fde1bb71661c1bfb0c5996bd3c824
planet-akash <akashyadavakash03@gmail.com>
2025-06-16 13:23:29 +0530

    dp on strings

---
+++ DP/DP on Strings/LongestCommonSubsequence.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// METHOD-1 Top Down
+    int t[1001][1001];
+    int solve(string &s1,string &s2,int i,int j){
+        if(i >= s1.length() || j >= s2.length()){
+            return 0;
+        }
+        
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(s1[i] == s2[j]){
+            return 1 + solve(s1,s2,i+1,j+1);
+        }
+
+        return t[i][j] = max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1));
+
+    }
+
+    int longestCommonSubsequence(string s1, string s2) {
+        memset(t,-1,sizeof(t));
+        return solve(s1,s2,0,0);
+    }
+
+// METHOD-2 Bottom Up
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        vector<vector<int>>t(n+1,vector<int>(m+1));
+        //first row and col will be 0
+        for(int row=0;row<n+1;row++){
+            t[row][0] = 0;
+        }
+
+        for(int col=0;col<m+1;col++){
+            t[0][col] = 0;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1])
+                t[i][j] = 1 + t[i-1][j-1];
+                else
+                t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+
+        return t[n][m]; 
+    }
