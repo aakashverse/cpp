6f6b105b670f8e9cf18e333d9f5a0a0ff6ee89d7
planet-akash <akashyadavakash03@gmail.com>
2025-06-21 23:19:23 +0530

    add reursive method

--- SORTING/BubbleSort.cpp
+++ SORTING/BubbleSort.cpp
@@ -1,18 +1,38 @@
 #include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[],int n){
+    if(n == 1)
+    return;
+
+    for(int i=0;i<n-1;i++){   //  one pass after which largest element is at last
+        if(arr[i] > arr[i+1])
+        swap(arr[i],arr[i+1]);
+    }
+
+    bubbleSort(arr,n-1); // for remaining array
+}
+
+void display(int arr[],int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
 int main(){
-    using namespace std;
     int arr[5]={5,4,9,2,1};
-    int i,j,n=5;
-
-    //FOR BUBBLE SORT
-
-    for(i=0; i<n-1; i++){     //for no of passes
-        for(j=0; j<n-1-i; j++){     //for swapping the element
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    // iteratively
+    for(int i=0; i<n; i++){     //for no of passes
+        for(int j=0; j<n-1-i; j++){     //for swapping the element
           if(arr[j]>arr[j+1])
           swap(arr[j],arr[j+1]);
         }
     }
-        for(i=0; i<=n-1; i++){
-            cout<<arr[i]<<" ";
-        }
+    display(arr,n);
+
+    //Recursive
+    bubbleSort(arr,n);
+    display(arr,n);
 }
